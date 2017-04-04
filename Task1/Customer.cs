f86506bf883560be39a8e8494267ca8fe6ee28c2
Krokvavets <vladislav.ramanovskiy@gmail.com>
2017-04-04 11:38:28 +0200

    comments

--- Task1/Customer.cs
+++ Task1/Customer.cs
@@ -15,8 +15,18 @@ namespace Task1
             ContactPhone = phone;
             Revenue = revenue;
         }
+        ///<summary>
+        /// Returns a string that represents the current object.
+        ///</summary>
+        ///<returns>A string that represents the current object.</returns>
         public override string ToString() => this.ToString("G", CultureInfo.CurrentCulture);
         public string ToString(string format) => this.ToString(format, CultureInfo.CurrentCulture);
+        ///<summary>
+        /// Formats the value of the current instance using the specified format.
+        ///</summary>
+        ///<param name="format">The format to use.</param>
+        ///<param name=" provider">The provider to use to format the value.</param>
+        ///<returns>The value of the current instance in the specified format.</returns>
         public string ToString(string format, IFormatProvider provider)
         {
             if (String.IsNullOrEmpty(format)) format = "G";
@@ -38,8 +48,6 @@ namespace Task1
                 default:
                     throw new FormatException(String.Format("The {0} format string is not supported.", format));
             }
-
-
         }
     }
 }
--- Task1/CustomerFormatter.cs
+++ Task1/CustomerFormatter.cs
@@ -14,13 +14,23 @@ namespace Task1
         {
             _parent = parent;
         }
-
+        ///<summary>
+        ///Returns an object that provides formatting services for the specified type.
+        ///</summary>
+        ///<param name="formatType">An object that specifies the type of format object to return.</param>
+        ///<returns>An instance of the object specified by formatType, if the IFormatProvider implementation can supply that type of object; otherwise, null.</returns>
         public object GetFormat(Type formatType)
         {
             if (formatType == typeof(ICustomFormatter)) return this;
             return null;
         }
-
+        ///<summary>
+        ///Converts the value of a specified object to an equivalent string representation using specified format and culture-specific formatting information.
+        ///</summary>
+        ///<param name="format">A format string containing formatting specifications.</param>
+        ///<param name="arg">An object to format.</param>
+        ///<param name="formatProvider">An object that supplies format information about the current instance.</param>
+        ///<returns>The string representation of the value of arg, formatted as specified by format and formatProvider.</returns>
         public string Format(string format, object arg, IFormatProvider formatProvider)
         {
             if (ReferenceEquals(arg, null)) throw new ArgumentException();
